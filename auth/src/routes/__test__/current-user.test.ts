03888ae8545ec669e67f85af1632d8bf8a8efd0c
savva <savva.lambin@gmail.com>
2024-05-02 14:45:19 +0100

    Refactor: cookie retrieval from global signin function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
-
-  const cookie = authResponse.get('Set-Cookie');
+  const cookie = await global.signin();
 
   if (cookie) {
     const response = await request(app)
@@ -23,4 +15,13 @@ it('responds with details about the current user', async () => {
   } else {
     fail('Signup did not provide a cookie');
   }
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
