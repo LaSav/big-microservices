75f4e541945609f3c7808c4feac779f0211a8624
savva <savva.lambin@gmail.com>
2024-05-03 23:11:27 +0100

    Add: SSR ingress configurations

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,29 @@
-const Index = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>landing page</h1>;
 };
 
-export default Index;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+    // Requests should be made to ingress-nginx
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // We are on the browser
+    // Requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
