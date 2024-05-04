071de15b48ca284155be073ea8032ee44cc61b24
savva <savva.lambin@gmail.com>
2024-05-04 10:43:42 +0100

    Add: buildClient helper function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,14 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>landing page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // We are on the server
-    // Requests should be made to ingress-nginx
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // We are on the browser
-    // Requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
