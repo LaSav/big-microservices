96b07dff856ea3b9f1a7c431a4f5666ca8cbee30
savva <savva.lambin@gmail.com>
2024-05-15 14:18:39 +0100

    Add: ticket service &  k8s configs, Update: Auth k8s config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,7 +8,7 @@ const start = async () => {
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be provided');
+    throw new Error('MONGO_URI must be defined');
   }
 
   try {
